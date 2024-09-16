0f77f4427b3805584e072883b1580ffbe8df2736
Bogdan Mezentsev <mezentsev.6ogdan@yandex.ru>
2024-09-16 17:40:52 +0300

    update laboratory 0 ASD

--- lab0_АиСД/task1/1.1.py
+++ lab0_АиСД/task1/1.1.py
@@ -1,6 +1,5 @@
 nums = list(map(int, input("Введите два числа через пробел: ").split()))
-if (-10**9 <= nums[0] <= 10**9) and (-10**9 <= nums[1] <= 10**9):
-    print(nums[0] + nums[1])
-else:
+while not ((-10**9 <= nums[0] <= 10**9) and (-10**9 <= nums[1] <= 10**9)):
     print("Введите число правильно!")
     nums = list(map(int, input("Введите два числа через пробел: ").split()))
+print(nums[0] + nums[1])
