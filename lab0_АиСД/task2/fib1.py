f1f9d13ca7652406090870548f6ca9cc8990a70b
Bogdan Mezentsev <mezentsev.6ogdan@yandex.ru>
2024-09-16 17:20:03 +0300

    update laboratory 0 ASD

--- lab0_АиСД/task2/fib1.py
+++ lab0_АиСД/task2/fib1.py
@@ -2,14 +2,16 @@ def fib1():
     with open('/Users/6ogdanmezentsev/PycharmProjects/ITMO_project/my_labs_ASD/lab0_АиСД/task2/input.txt', 'r') as file:
         n = int(file.readline())
         file.close()
+    if 0 <= n <= 45:
+        a, b = 0, 1
+        for i in range(n):
+            a, b = b, a + b
 
-    a, b = 0, 1
-    for i in range(n):
-        a, b = b, a + b
-
-    with open('/Users/6ogdanmezentsev/PycharmProjects/ITMO_project/my_labs_ASD/lab0_АиСД/task2/output.txt', 'w') as file:
-        file.write(str(a))
-        file.close()
+        with open('/Users/6ogdanmezentsev/PycharmProjects/ITMO_project/my_labs_ASD/lab0_АиСД/task2/output.txt', 'w') as file:
+            file.write(str(a))
+            file.close()
+    else:
+        print("Введите число корректно!")
 
 fib1()
 
