5305f2c11b90bfb9646d2ac08adb9e16a5e00d18
Bogdan Mezentsev <mezentsev.6ogdan@yandex.ru>
2024-09-21 16:18:46 +0300

    fix lab0

--- lab0_AiSD/task2/fib1.py
+++ lab0_AiSD/task2/fib1.py
@@ -1,5 +1,5 @@
 def fib1():
-    with open('/my_labs_ASD/lab0_АиСД/task2/input1.txt', 'r') as file:
+    with open('/my_labs_ASD/lab0_AiSD/task2/input1.txt', 'r') as file:
         n = int(file.readline())
         file.close()
     if 0 <= n <= 45:
@@ -7,7 +7,7 @@ def fib1():
         for i in range(n):
             a, b = b, a + b
 
-        with open('/my_labs_ASD/lab0_АиСД/task2/output1.txt', 'w') as file:
+        with open('/my_labs_ASD/lab0_AiSD/task2/output1.txt', 'w') as file:
             file.write(str(a))
             file.close()
     else:
